50e6b1058378a28f83bb0035673c66ad67ee3605
Mike Samuel <mikesamuel@gmail.com>
2018-06-09 17:02:10 +0200

    more polyfill tests

---
+++ test/data/babel/not-polyfills/code.js
@@ -0,0 +1,14 @@
+'use strict';
+
+;
+
+// None of these are polyfills
+Require('module-keys/cjs').polyfill(module, require);
+require('nodule-keys/cjs').polyfill(module, require);
+require('module-keys/cjs').polyfool(module, require);
+require.polyfill(module, require);
+require('module-keys/cjs').polyfill(module);
+require('module-keys/cjs').polyfill(mule, require);
+require('module-keys/cjs').polyfill(require, module);
+require('module-keys/cjs').polyfill(module, require, 'Why am I here?');
+// End of bad polyfills
---
+++ test/data/babel/not-polyfills/output.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('module-keys/cjs').polyfill(module, require);
+
+;
+
+// None of these are polyfills
+Require('module-keys/cjs').polyfill(module, require);
+require('nodule-keys/cjs').polyfill(module, require);
+require('module-keys/cjs').polyfool(module, require);
+require.polyfill(module, require);
+require('module-keys/cjs').polyfill(module);
+require('module-keys/cjs').polyfill(mule, require);
+require('module-keys/cjs').polyfill(require, module);
+require('module-keys/cjs').polyfill(module, require, 'Why am I here?');
+// End of bad polyfills
